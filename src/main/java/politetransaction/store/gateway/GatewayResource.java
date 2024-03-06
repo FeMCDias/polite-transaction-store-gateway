369269c96fd682fdabae1d4eb065773513c862cd
FeMCDias <femcdias@gmail.com>
2024-03-06 11:11:35 -0300

    feat: god's gate

---
+++ src/main/java/politetransaction/store/gateway/GatewayResource.java
@@ -0,0 +1,11 @@
+package politetransaction.store.gateway;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GatewayResource{
+    @GetMapping("/hello")
+    public String info(){
+        return "Hello, this is the gateway service!";
+    }
+}
